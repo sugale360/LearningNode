678d71a4bb4eb907e4b63e3b6a17ce0b5496f792
Sagar Ugale <sugale360@gmail.com>
2024-02-29 20:54:16 +0530

    [SU]: Added fs crud operations.

--- index.js
+++ index.js
@@ -37,10 +37,26 @@
 //     console.log("Invalid input");
 // }
 //---------------------------------------------------
+// const fs = require('fs');
+// const path = require('path');
+// const dirPath = path.join(__dirname,'files')
+
+// for(i=0;i<5;i++){
+//     fs.writeFileSync(`${dirPath}/Hello${i}`,"Hello there")
+// }
+//---------------------------------------------------
 const fs = require('fs');
 const path = require('path');
-const dirPath = path.join(__dirname,'files')
-
-for(i=0;i<5;i++){
-    fs.writeFileSync(`${dirPath}/Hello${i}`,"Hello there")
-}
+const dir = path.join(__dirname,'crud');
+const filePath = `${dir}/apple.txt`;
+// fs.writeFileSync(filePath,'This is first text');
+// fs.readFile(filePath,'utf8',(err, item)=>{
+//     console.log(item);
+// })
+// fs.appendFile(filePath, 'and this is second text',(err)=>{
+//     if(!err) console.log('file is updated');
+// });
+// fs.rename(filePath,`${dir}/fruit.txt`,(err)=>{
+//     if(!err) console.log("File renamed");
+// })
+// fs.unlinkSync(`${dir}/fruit.txt`);
