f4800201cfaeb8892f72fb10aeb6215175ef30aa
Sagar <sugale360@gmail.com>
2024-04-10 20:45:04 +0530

    [SU]: Added separate file to connect to mongodb.

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -1,14 +1,16 @@
-const { MongoClient } = require("mongodb");
-const url = "mongodb://0.0.0.0";
-const client = new MongoClient(url);
-const database = "e-comm";
+const connectDB = require('./mongoDB')
 
-async function getData() {
-  let result = await client.connect();
-  let db = result.db(database);
-  let collection = db.collection('products');
-  let response = await collection.find({}).toArray();
-  console.log(response);
+// with promise
+// connectDB().then((res)=>{
+// res.find({name:'m 40'}).toArray().then((data)=> console.warn(data))
+// });
+// console.warn(connectDB());
+
+// with async await
+const main = async () => {
+  let data = await connectDB();
+  data = await data.find().toArray();
+console.warn(data);
 }
 
-getData();
+main();
