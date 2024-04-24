367781dd5af79cddfc812d189fcf1eeaddaa0e71
Sagar <sugale360@gmail.com>
2024-04-24 21:32:45 +0530

    [SU]

--- Mongoose/index.js
+++ Mongoose/index.js
@@ -1,15 +1,16 @@
 const { default: mongoose } = require("mongoose");
 
-const main = async () => {
+const saveInDB = async () => {
   await mongoose.connect("mongodb://0.0.0.0/e-comm");
   const ProductSchema = new mongoose.Schema({ 
     name: String,
-    price: Number
+    price: Number,
+    brand: String,
+    Category: String
  });
 
 const ProductsModel = mongoose.model('products', ProductSchema);
-let data = new  ProductsModel({name:'gt2',price:1000});
+let data = new  ProductsModel({name:'note pro',price:25000, brand:'mi',Category:'mobile'});
 let result = await data.save();
 console.log(result);
 };
-main();
